3af836213f1427f7d21b76417dd14ed9dad2ea15
Sivani Saravanan <31069721+inavis@users.noreply.github.com>
2021-10-20 16:05:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+var xhr = new XMLHttpRequest();
+
+xhr.open("GET","https://jsonplaceholder.typicode.com/users");
+xhr.send();
+
+xhr.onload=function(){
+    if(xhr.status >=200 && xhr.status<300){
+        var data = JSON.parse(this.response);
+        //console.log(data)
+        for(var i=0;i<data.length;i++){
+            console.log(`
+            ID: ${data[i].id}
+            Name: ${data[i].name}
+            Email id: ${data[i].email}
+            City: ${data[i].address.city}
+            Phone number : ${data[i].phone}
+            
+            `);
+        }
+    }else{
+        console.log("Data not available");
+    }
+}
